fad732d8219fb6a67d1cf0b09423d69cb0de45d4
Artii15 <artur.kowalski15@gmail.com>
2017-01-07 23:07:28 +0100

    Queue messages lock initialization

--- server/src/Storage/Files/FileQueuesStorage.cs
+++ server/src/Storage/Files/FileQueuesStorage.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Threading;
+using ServiceStack.Net30.Collections.Concurrent;
 
 namespace Server.Storage.Files
 {
 	public class FileQueuesStorage : QueuesStorage
 	{
-		Paths Paths;
+		readonly Paths Paths;
+		readonly ConcurrentDictionary<string, ReaderWriterLockSlim> MessagesLocks;
 
-		public FileQueuesStorage(Paths paths)
+		public FileQueuesStorage(Paths paths,
+		                        ConcurrentDictionary<string, ReaderWriterLockSlim> messagesLocks)
 		{
 			Paths = paths;
+			MessagesLocks = messagesLocks;
 		}
 		
 		public void Allocate(string queueName)
+		{
+			createQueueFiles(queueName);
+			MessagesLocks.TryAdd(queueName, new ReaderWriterLockSlim());
+		}
+
+		void createQueueFiles(string queueName)
 		{
 			Directory.CreateDirectory(Paths.GetTopicsPath(queueName));
 			Directory.CreateDirectory(Paths.GetMessagesPath(queueName));
