d23550c3c5b04ac5266a7ad9a26a8d6781c89437
Artii15 <artur.kowalski15@gmail.com>
2017-01-10 20:33:50 +0100

    CreateTopicService implementation

--- server/src/Services/Topics/Create/CreateTopicService.cs
+++ server/src/Services/Topics/Create/CreateTopicService.cs
@@ -1,12 +1,20 @@
-﻿using ServiceStack.ServiceInterface;
+﻿using Server.Storage;
+using ServiceStack.ServiceInterface;
 
 namespace Server.Services.Topics.Create
 {
 	public class CreateTopicService: Service
 	{
+		TopicsStorage TopicsStorage;
+
+		public CreateTopicService(TopicsStorage topicsStorage)
+		{
+			TopicsStorage = topicsStorage;
+		}
+
 		public CreateTopicResponse Post(CreateTopic request)
 		{
-			
+			TopicsStorage.Create(request.QueueName, request.TopicName);
 			return new CreateTopicResponse();
 		}
 	}
