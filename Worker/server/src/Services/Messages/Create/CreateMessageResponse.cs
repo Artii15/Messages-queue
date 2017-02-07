faca0a02bbe0e7340480e1aafeeab1c09e3fa58c
Artii15 <artur.kowalski15@gmail.com>
2017-02-07 23:40:02 +0100

    Create message service

---
+++ Worker/server/src/Services/Messages/Create/CreateMessageResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Server.Services.Messages.Create
+{
+	public class CreateMessageResponse
+	{
+	}
+}
---
+++ Worker/server/src/Services/Messages/Create/CreateMessageService.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStack.ServiceHost;
+
+namespace Server.Services.Messages.Create
+{
+	public class CreateMessageService: IService
+	{
+		public CreateMessageResponse Post(CreateMessage request)
+		{
+			return new CreateMessageResponse();
+		}
+	}
+}
