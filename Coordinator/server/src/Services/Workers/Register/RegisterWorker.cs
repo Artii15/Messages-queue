1fd88e1b4b6d3b34e3420d4f93ec288daf2ba2cc
Pyre540 <kamil540@gazeta.pl>
2017-02-08 17:32:53 +0100

    register worker service

---
+++ Coordinator/server/src/Services/Workers/Register/RegisterWorker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Server
+{
+	public class RegisterWorker
+	{
+		public RegisterWorker()
+		{
+		}
+	}
+}
---
+++ Coordinator/server/src/Services/Workers/Register/RegisterWorkerResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Server
+{
+	public class RegisterWorkerResponse
+	{
+		public RegisterWorkerResponse()
+		{
+		}
+	}
+}
