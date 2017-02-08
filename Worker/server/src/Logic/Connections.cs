4f35cb0e031b0093acbf750780ea8e18f8e395d1
Artii15 <artur.kowalski15@gmail.com>
2017-02-08 21:02:43 +0100

    Connections storage abstraction

---
+++ Worker/server/src/Logic/Connections.cs
@@ -0,0 +1,24 @@
+﻿using ServiceStack.OrmLite;
+using System.Collections.Concurrent;
+using ServiceStack.OrmLite.Sqlite;
+
+namespace Server.Logic
+{
+	public class Connections
+	{
+		ConcurrentDictionary<string, IDbConnectionFactory> Queues;
+
+		public Connections()
+		{
+			Queues = new ConcurrentDictionary<string, IDbConnectionFactory>();
+		}
+
+		public IDbConnectionFactory ConnectToQueue(string queueName)
+		{
+			var queueStoragePath = $"queues/{queueName}.sqlite";
+			return Queues.GetOrAdd(queueName,
+			                       new OrmLiteConnectionFactory($"Data Source={queueStoragePath};Version=3;",
+														 SqliteOrmLiteDialectProvider.Instance));
+		}
+	}
+}
