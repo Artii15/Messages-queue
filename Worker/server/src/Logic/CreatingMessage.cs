c47b50cbf382c4cae2d4e946e9949a2bf7bdbb93
Artii15 <artur.kowalski15@gmail.com>
2017-02-08 21:33:45 +0100

    Waiting processes notifications

--- Worker/server/src/Logic/CreatingMessage.cs
+++ Worker/server/src/Logic/CreatingMessage.cs
@@ -1,28 +1,38 @@
-﻿using System.Collections.Concurrent;
-using Server.Entities;
+﻿using Server.Entities;
 using Server.Services.Messages.Create;
 using ServiceStack.OrmLite;
+using System.Threading;
 
 namespace Server.Logic
 {
 	public class CreatingMessage
 	{
 		readonly Connections Connections;
+		readonly Locks Locks;
 
-		public CreatingMessage(Connections connections)
+		public CreatingMessage(Connections connections, Locks locks)
 		{
 			Connections = connections;
+			Locks = locks;
 		}
 
 		public void Create(CreateMessage request)
 		{
 			var connection = Connections.ConnectToInitializedQueue(request.QueueName);
+			var queueLock = Locks.TakeQueueLock(request.QueueName);
+
+			Monitor.Enter(queueLock);
+
 			connection.Insert(new QueueMessage
 			{
 				Author = request.Author,
 				Content = request.Content,
 				Readed = false
 			});
+
+			Monitor.PulseAll(queueLock);
+			Monitor.Exit(queueLock);
+
 			connection.Close();
 		}
 	}
