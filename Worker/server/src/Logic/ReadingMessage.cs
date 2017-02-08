b9b83041667ca794f5eb9df1d1be7fa580d8fd4d
Artii15 <artur.kowalski15@gmail.com>
2017-02-08 20:39:23 +0100

    Waiting for message creating

--- Worker/server/src/Logic/ReadingMessage.cs
+++ Worker/server/src/Logic/ReadingMessage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Data;
+using System.Threading;
 using Server.Entities;
 using ServiceStack.OrmLite;
 
@@ -9,10 +10,13 @@ namespace Server.Logic
 	public class ReadingMessage
 	{
 		readonly ConcurrentDictionary<string, IDbConnectionFactory> Queues;
+		readonly ConcurrentDictionary<string, object> QueuesLocks;
 
-		public ReadingMessage(ConcurrentDictionary<string, IDbConnectionFactory> queues)
+		public ReadingMessage(ConcurrentDictionary<string, IDbConnectionFactory> queues,
+		                      ConcurrentDictionary<string, object> queuesLocks)
 		{
 			Queues = queues;
+			QueuesLocks = queuesLocks;
 		}
 
 		public string ReadNextFrom(string queueName)
@@ -21,25 +25,36 @@ namespace Server.Logic
 			if (Queues.TryGetValue(queueName, out queueConnectionFactory))
 			{
 				var connection = queueConnectionFactory.Open();
-				var message = ReadMessage(connection);
+				var queueLock = QueuesLocks.GetOrAdd(queueName, new object());
+				var message = ReadMessage(connection, queueLock);
 				connection.Close();
 
 				return message;
 			}
-			else
-			{
-				throw new ArgumentException("Specified queue not found");
-			}
+			throw new ArgumentException("Specified queue not found");
 		}
 
-		string ReadMessage(IDbConnection connection)
+		string ReadMessage(IDbConnection connection, object queueLock)
+		{
+			Monitor.Enter(queueLock);
+
+			var messageToReturn = ReadFromDb(connection);
+			while (messageToReturn == null)
+			{
+				Monitor.Wait(queueLock);
+				messageToReturn = ReadFromDb(connection);
+			}
+			Monitor.Exit(queueLock);
+
+			return messageToReturn.Content;
+		}
+
+		QueueMessage ReadFromDb(IDbConnection connection)
 		{
 			var query = connection.CreateExpression<QueueMessage>()
 						  .Where(message => !message.Readed)
 						  .OrderByDescending(message => message.Id);
-			var messageToReturn = connection.FirstOrDefault(query);
-
-			return messageToReturn.Content;
+			return connection.FirstOrDefault(query);
 		}
 	}
 }
