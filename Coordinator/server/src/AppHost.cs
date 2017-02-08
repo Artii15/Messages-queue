b3bd9eb7c471e2e28fb903e85f315ba7059196eb
Pyre540 <kamil540@gazeta.pl>
2017-02-08 12:40:48 +0100

    apphost setup

--- Coordinator/server/src/AppHost.cs
+++ Coordinator/server/src/AppHost.cs
@@ -1,10 +1,8 @@
 ﻿using Funq;
-using ServiceStack.CacheAccess;
-using ServiceStack.CacheAccess.Providers;
 using ServiceStack.MiniProfiler;
 using ServiceStack.MiniProfiler.Data;
 using ServiceStack.OrmLite;
-using ServiceStack.OrmLite.Sqlite;
+using ServiceStack.OrmLite.PostgreSQL;
 using ServiceStack.ServiceInterface;
 using ServiceStack.ServiceInterface.Auth;
 using ServiceStack.ServiceInterface.Validation;
@@ -20,6 +18,19 @@ namespace Server
     {
 		private readonly bool m_debugEnabled = true;
 
+		private const string ENV_PG_IP = "PG_IP";
+		private const string ENV_PG_USER = "PG_USER";
+		private const string ENV_PG_DB = "PG_DB";
+		private const string ENV_PG_PASS = "PG_PASS";
+		private const string ENV_PG_PORT = "PG_PORT";
+
+		string m_pgIp;
+		string m_pgUser;
+		string m_pgDb;
+		string m_pgPass;
+		string m_pgPort;
+		string m_pgConnString;
+
         public AppHost ()
             : base ("Server HttpListener", typeof (AppHost).Assembly)
         {
@@ -27,6 +38,8 @@ namespace Server
 
 		public override void Configure (Container container)
         {
+			LoadConfigEnv();
+
 			RequestFilters.Add((req, resp, requestDto) => {
 				var log = LogManager.GetLogger(GetType());
 				log.Info (string.Format ("REQ {0}: {1} {2} {3} {4} {5}", DateTimeOffset.Now.Ticks, req.HttpMethod, 
@@ -47,18 +60,16 @@ namespace Server
 			Plugins.Add(new SessionFeature());
 			Plugins.Add(new RequestLogsFeature());
 
-			container.Register<ICacheClient> (new MemoryCacheClient ());
+			container.Register<IDbConnectionFactory>(
+				new OrmLiteConnectionFactory(m_pgConnString, PostgreSQLDialectProvider.Instance)
+				{
+					ConnectionFilter = x => new ProfiledDbConnection(x, Profiler.Current)
+				});
 
-            container.Register<IDbConnectionFactory> (
-				new OrmLiteConnectionFactory (@"Data Source=db.sqlite;Version=3;",
-					SqliteOrmLiteDialectProvider.Instance)
-                    {
-                        ConnectionFilter = x => new ProfiledDbConnection (x, Profiler.Current)
-                    });
+			//Use OrmLite DB Connection to persist the UserAuth and AuthProvider info
+			container.Register<IUserAuthRepository>(c => new OrmLiteAuthRepository(c.Resolve<IDbConnectionFactory>()));
 
-            //Use OrmLite DB Connection to persist the UserAuth and AuthProvider info
-            container.Register<IUserAuthRepository> (c => new OrmLiteAuthRepository (c.Resolve<IDbConnectionFactory> ()));
-            
+
 			Plugins.Add (new ValidationFeature ());
 
             var config = new EndpointHostConfig ();
@@ -72,5 +83,17 @@ namespace Server
 
             SetConfig (config);
         }
+
+		void LoadConfigEnv()
+		{
+			m_pgIp = Environment.GetEnvironmentVariable(ENV_PG_IP);
+			m_pgUser = Environment.GetEnvironmentVariable(ENV_PG_USER);
+			m_pgDb = Environment.GetEnvironmentVariable(ENV_PG_DB);
+			m_pgPass = Environment.GetEnvironmentVariable(ENV_PG_PASS);
+			m_pgPort = Environment.GetEnvironmentVariable(ENV_PG_PORT);
+
+			m_pgConnString = string.Format("User ID={0};Password={1};Host={2};Port={3};Database={4};SSL=True",
+				m_pgUser, m_pgPass, m_pgIp, m_pgPort, m_pgDb);
+		}
     }
 }
