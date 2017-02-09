ed23392f31527b72b42b4ce8b37bba2821aabba3
Artii15 <artur.kowalski15@gmail.com>
2017-02-09 14:51:02 +0100

    Topics entities

--- Worker/server/src/Entities/Announcement.cs
+++ Worker/server/src/Entities/Announcement.cs
@@ -1,6 +1,14 @@
-﻿namespace Server.Entities
+﻿using System;
+using ServiceStack.DataAnnotations;
+
+namespace Server.Entities
 {
 	public class Announcement
 	{
+		[AutoIncrement]
+		public int Id { get; set; }
+		public string Content { get; set; }
+		[Index]
+		public DateTime CreationTime { get; set; }
 	}
 }
--- Worker/server/src/Entities/Subscription.cs
+++ Worker/server/src/Entities/Subscription.cs
@@ -1,9 +1,17 @@
-﻿namespace Server.Entities
+﻿using System;
+using ServiceStack.DataAnnotations;
+
+namespace Server.Entities
 {
+	[CompositeIndex(true, new[] {"SubscriberId", "TopicId"})]
 	public class Subscription
 	{
-		public Subscription()
-		{
-		}
+		[AutoIncrement]
+		public int Id;
+		public int SubscriberId;
+		public int TopicId;
+		public DateTime CreationTime;
+		[References(typeof(Announcement))]
+		public int? LastAnnouncementId;
 	}
 }
