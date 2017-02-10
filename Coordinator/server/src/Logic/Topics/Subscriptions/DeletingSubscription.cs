9d872378656396572ed29e5ede957c65f5f43f67
Pyre540 <kamil540@gazeta.pl>
2017-02-10 18:35:48 +0100

    delete subscription

---
+++ Coordinator/server/src/Logic/Topics/Subscriptions/DeletingSubscription.cs
@@ -0,0 +1,55 @@
+﻿using System.Data;
+using RestSharp;
+using System;
+
+namespace Server
+{
+	public class DeletingSubscription
+	{
+		readonly IDbConnection DBConnection;
+		const int TIMEOUT = 30000;
+
+		public DeletingSubscription(IDbConnection dbConnection)
+		{
+			DBConnection = dbConnection;
+		}
+
+		public void Delete(DeleteSubscription request, int subscriberId)
+		{ 
+			if (!TopicsQueries.TopicExists(DBConnection, request.TopicName))
+				throw new TopicNotExistsException();
+			else
+			{
+				var topic = TopicsQueries.getTopicByName(DBConnection, request.TopicName);
+				var worker = WorkerQueries.GetWorkerById(DBConnection, topic.Worker);
+				var coworker = WorkerQueries.GetWorkerById(DBConnection, topic.Cooperator);
+
+				if (worker.Alive)
+				{
+					var response = PropagateRequest(request, subscriberId, worker, coworker);
+					if (response.ResponseStatus == ResponseStatus.TimedOut ||
+						response.ResponseStatus == ResponseStatus.Error)
+						PropagateRequestToCoworker(request, subscriberId, coworker);
+				}
+				else
+					PropagateRequestToCoworker(request, subscriberId, coworker);
+			}
+		}
+
+		IRestResponse PropagateRequest(DeleteSubscription request, int subscriberId, Worker worker, Worker coworker)
+		{
+			var client = new RestClient($"http://{worker.Address}");
+			client.Timeout = TIMEOUT;
+			var requestToSend = new RestRequest($"topics/{request.TopicName}/subscriptions/{subscriberId}", Method.DELETE);
+			requestToSend.AddParameter("Cooperator", coworker.Address);
+			return client.Execute(requestToSend);
+		}
+
+		void PropagateRequestToCoworker(DeleteSubscription request, int subscriberId, Worker coworker)
+		{
+			var coworkerClient = new RestClient($"http://{coworker.Address}");
+			var coworkerRequestToSend = new RestRequest($"topics/{request.TopicName}/subscriptions/{subscriberId}", Method.DELETE);
+			coworkerClient.Execute(coworkerRequestToSend);
+		}
+	}
+}
