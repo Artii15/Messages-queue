3d256650508bd225232d24f74f4e7935345c499e
Pyre540 <kamil540@gazeta.pl>
2017-02-10 16:28:04 +0100

    create subscription service

---
+++ Coordinator/server/src/Logic/Topics/Subscriptions/CreatingSubscription.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using RestSharp;
+using System;
+
+namespace Server
+{
+	public class CreatingSubscription
+	{
+		readonly IDbConnection DBConnection;
+		const int TIMEOUT = 30000;
+
+		public CreatingSubscription(IDbConnection dbConnection)
+		{
+			DBConnection = dbConnection;
+		}
+
+		public void Create(CreateSubscription request)
+		{
+		}
+	}
+}
---
+++ Coordinator/server/src/Services/Topics/Subscriptions/Create/CreateSubscriptionService.cs
@@ -0,0 +1,35 @@
+﻿using ServiceStack.Common.Web;
+using ServiceStack.Common;
+using ServiceStack.OrmLite;
+using ServiceStack.ServiceInterface;
+using System.Net;
+using System.Data;
+
+namespace Server
+{
+	[Authenticate]
+	public class CreateSubscriptionService : Service
+	{
+		readonly CreatingSubscription CreatingSubscription;
+
+		public CreateSubscriptionService()
+		{
+			CreatingSubscription = new CreatingSubscription(Db);
+		}
+
+		public object Post(CreateSubscription request)
+		{
+			try
+			{
+				CreatingSubscription.Create(request);
+			}
+			catch (TopicNotExistsException)
+			{
+				return new HttpError(HttpStatusCode.NotFound, $"Queue {request.TopicName} not exists");
+			}
+
+			return new HttpResult(new CreateSubscriptionResponse(), HttpStatusCode.Created);
+
+		}
+	}
+}
