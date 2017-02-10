5c7ea158a4c9a2d64508657f2832dbefb7fbfba6
Artii15 <artur.kowalski15@gmail.com>
2017-02-10 16:04:45 +0100

    Propagating delete subscription request

--- Worker/server/src/Logic/DeletingSubscription.cs
+++ Worker/server/src/Logic/DeletingSubscription.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using RestSharp;
+using Server.Entities;
 using Server.Services.Subscriptions.Delete;
+using ServiceStack.OrmLite;
 
 namespace Server.Logic
 {
 	public class DeletingSubscription
 	{
-		public DeletingSubscription()
+		readonly Connections Connections;
+
+		public DeletingSubscription(Connections connections)
 		{
+			Connections = connections;
 		}
 
-		public void Delete(DeleteSubscription deleteSubscription)
+		public void Delete(DeleteSubscription request)
 		{
+			Propagate(request);
+			using (var connection = Connections.ConnectToInitializedTopic(request.TopicName))
+			{
+				connection.DeleteById<Subscriber>(request.SubscriberId);
+			}
 
+		}
+
+		void Propagate(DeleteSubscription request)
+		{
+			if (!string.IsNullOrEmpty(request.Cooperator))
+			{
+				var client = new RestClient(request.Cooperator);
+				var requestToSend = new RestRequest($"topics/{request.TopicName}/subscriptions/{request.SubscriberId}", Method.DELETE);
+				client.Execute(requestToSend);
+			}
 		}
 	}
 }
