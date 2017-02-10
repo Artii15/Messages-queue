f53bb5f6e097ac61a63d88c958d19ad9f586d15c
Artii15 <artur.kowalski15@gmail.com>
2017-02-10 13:08:55 +0100

    Requests propagation

--- Worker/server/src/Logic/DeletingAnnouncement.cs
+++ Worker/server/src/Logic/DeletingAnnouncement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RestSharp;
 using Server.Entities;
 using Server.Queries;
 using Server.Services.Announcements.Delete;
@@ -22,14 +23,29 @@ namespace Server.Logic
 				var subscriber = connection.GetById<Subscriber>(request.SubscriberId);
 				var announcement = connection.First(NextAnnouncement.make(connection, subscriber));
 
+				Propagate(request);
+
 				if (announcement.Id != request.AnnouncementId)
 				{
 					throw new ArgumentException("Invalid announcement");
 				}
 
 				connection.UpdateOnly(new Subscriber { LastAnnouncementId = announcement.Id },
-				                      subscription => new { subscription.LastAnnouncementId },
-				                      subscription => subscription.Id == subscriber.Id);
+									  subscription => new { subscription.LastAnnouncementId },
+									  subscription => subscription.Id == subscriber.Id);
+			}
+		}
+
+		void Propagate(DeleteAnnouncement request)
+		{
+			if (!string.IsNullOrEmpty(request.Cooperator))
+			{
+				var client = new RestClient(request.Cooperator);
+
+				var requestToSend = new RestRequest($"topics/{request.TopicName}/announcements/{request.AnnouncementId}", Method.DELETE);
+				requestToSend.AddBody(new { request.SubscriberId });
+
+				client.Execute(requestToSend);
 			}
 		}
 	}
