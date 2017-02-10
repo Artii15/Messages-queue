4929544f9b8f47e08443254a091057b7e2479a0c
Pyre540 <kamil540@gazeta.pl>
2017-02-10 15:08:20 +0100

    create topic service

---
+++ Coordinator/server/src/Logic/Topics/CreatingTopic.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using RestSharp;
+using System;
+
+namespace Server
+{
+	public class CreatingTopic
+	{
+		readonly IDbConnection DBConnection;
+		const int TIMEOUT = 30000;
+
+		public CreatingTopic(IDbConnection dbConnection)
+		{
+			DBConnection = dbConnection;
+		}
+
+		public void Create(CreateTopic request)
+		{
+		}
+	}
+}
---
+++ Coordinator/server/src/Services/Topics/Create/CreateTopicService.cs
@@ -0,0 +1,47 @@
+﻿using ServiceStack.Common.Web;
+using ServiceStack.Common;
+using ServiceStack.OrmLite;
+using ServiceStack.ServiceInterface;
+using System.Net;
+using System.Data;
+
+namespace Server
+{
+	[Authenticate]
+	public class CreateTopicService : Service
+	{
+		readonly CreatingTopic CreatingTopic;
+
+		public CreateTopicService()
+		{
+			CreatingTopic = new CreatingTopic(Db);
+			Db.CreateTableIfNotExists<Topic>();
+		}
+
+		public object Post(CreateTopic request)
+		{
+			IDbTransaction transaction = Db.OpenTransaction();
+			try
+			{
+				CreatingTopic.Create(request);
+			}
+			catch (TopicAlreadyExistsException)
+			{
+				return new HttpError(HttpStatusCode.Conflict, $"Topic {request.Name} already exists");
+			}
+			finally
+			{
+				transaction.Commit();
+			}
+
+			return new HttpResult(new CreateQueueResponse())
+			{
+				StatusCode = HttpStatusCode.Created,
+				Headers =
+							   {
+					{HttpHeaders.Location, base.Request.AbsoluteUri.CombineWith(request.Name)}
+							   }
+			};
+		}
+	}
+}
