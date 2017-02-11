59c1ca60d9424d271db0f097375457e867f3591e
Artii15 <artur.kowalski15@gmail.com>
2017-02-11 20:33:58 +0100

    Address validation

--- Worker/server/src/Program.cs
+++ Worker/server/src/Program.cs
@@ -20,6 +20,14 @@ namespace Server
 				Console.Error.WriteLine ("Invalid address format");
 				Environment.Exit(2);
 			}
+			if (listenAddress.Contains("127.0.0.1")
+			   || listenAddress.Contains("localhost")
+			   || listenAddress.Contains("://*")
+			   || listenAddress.Contains("0.0.0.0"))
+			{
+				Console.Error.WriteLine("Use specific address visible outside");
+				Environment.Exit(3);
+			}
 
 			var appHost = new AppHost(listenAddress);
             appHost.Init ();            
