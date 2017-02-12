aae27ea8d7c5eaf98c87d28c265ba7a658728276
Pyre540 <kamil540@gazeta.pl>
2017-02-12 18:00:32 +0100

    create announcement logic

--- Coordinator/server/src/Logic/Topics/Announcements/CreatingAnnouncement.cs
+++ Coordinator/server/src/Logic/Topics/Announcements/CreatingAnnouncement.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using RestSharp;
 
 namespace Server
@@ -15,6 +16,45 @@ namespace Server
 
 		public void Create(CreateAnnouncement request)
 		{
+			if (!TopicsQueries.TopicExists(DBConnection, request.TopicName))
+				throw new TopicNotExistsException();
+			else
+			{
+				var topic = TopicsQueries.getTopicByName(DBConnection, request.TopicName);
+				var worker = WorkerQueries.GetWorkerById(DBConnection, topic.Worker);
+				var coworker = WorkerQueries.GetWorkerById(DBConnection, topic.Cooperator);
+
+				if (worker.Alive)
+				{
+					var response = PropagateRequest(request, worker, coworker);
+					if (response.ResponseStatus == ResponseStatus.TimedOut ||
+						response.ResponseStatus == ResponseStatus.Error)
+						PropagateRequestToCoworker(request, coworker);
+				}
+				else
+					PropagateRequestToCoworker(request, coworker);
+			}
+		}
+
+		IRestResponse PropagateRequest(CreateAnnouncement request, Worker worker, Worker coworker)
+		{
+			var client = new RestClient($"http://{worker.Address}");
+			client.Timeout = TIMEOUT;
+			var requestToSend = new RestRequest($"/topics/{request.TopicName}/announcements", Method.POST);
+			requestToSend.AddParameter("Content", request.Content);
+			requestToSend.AddParameter("CreationTime", DateTime.UtcNow);
+			if (coworker.Alive)
+				requestToSend.AddParameter("Cooperator", coworker.Address);
+			return client.Execute(requestToSend);
+		}
+
+		void PropagateRequestToCoworker(CreateAnnouncement request, Worker coworker)
+		{
+			var coworkerClient = new RestClient($"http://{coworker.Address}");
+			var coworkerRequestToSend = new RestRequest($"/topics/{request.TopicName}/announcements", Method.POST);
+			coworkerRequestToSend.AddParameter("Content", request.Content);
+			coworkerRequestToSend.AddParameter("CreationTime", DateTime.UtcNow);
+			coworkerClient.Execute(coworkerRequestToSend);
 		}
 	}
 }
