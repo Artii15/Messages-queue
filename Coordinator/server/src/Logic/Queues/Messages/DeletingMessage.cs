086ea5ce38e0a35d38265754f8b347bb49c0716b
Pyre540 <kamil540@gazeta.pl>
2017-02-12 15:43:38 +0100

    delete message logic

--- Coordinator/server/src/Logic/Queues/Messages/DeletingMessage.cs
+++ Coordinator/server/src/Logic/Queues/Messages/DeletingMessage.cs
@@ -15,6 +15,41 @@ namespace Server
 
 		public void Delete(DeleteMessage request)
 		{
+			if (!QueuesQueries.QueueExists(DBConnection, request.QueueName))
+				throw new QueueNotExistsException();
+			else
+			{
+				var queue = QueuesQueries.getQueueByName(DBConnection, request.QueueName);
+				var worker = WorkerQueries.GetWorkerById(DBConnection, queue.Worker);
+				var coworker = WorkerQueries.GetWorkerById(DBConnection, queue.Cooperator);
+
+				if (worker.Alive)
+				{
+					var response = PropagateRequest(request, worker, coworker);
+					if (response.ResponseStatus == ResponseStatus.TimedOut ||
+						response.ResponseStatus == ResponseStatus.Error)
+						PropagateRequestToCoworker(request, coworker);
+				}
+				else
+					PropagateRequestToCoworker(request, coworker);
+			}
+		}
+
+		IRestResponse PropagateRequest(DeleteMessage request, Worker worker, Worker coworker)
+		{
+			var client = new RestClient($"http://{worker.Address}");
+			client.Timeout = TIMEOUT;
+			var requestToSend = new RestRequest($"/queues/{request.QueueName}/messages/{request.MessageId}", Method.DELETE);
+			if (coworker.Alive)
+				requestToSend.AddParameter("Cooperator", coworker.Address);
+			return client.Execute(requestToSend);
+		}
+
+		void PropagateRequestToCoworker(DeleteMessage request, Worker coworker)
+		{
+			var coworkerClient = new RestClient($"http://{coworker.Address}");
+			var coworkerRequestToSend = new RestRequest($"/queues/{request.QueueName}/messages/{request.MessageId}", Method.DELETE);
+			coworkerClient.Execute(coworkerRequestToSend);
 		}
 	}
 }
