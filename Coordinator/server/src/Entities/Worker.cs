65bc6f7a838644ffadfb5cf45a6ad634d355a947
Pyre540 <kamil540@gazeta.pl>
2017-02-13 16:51:52 +0100

    removed autoincrement id from worker

--- Coordinator/server/src/Entities/Worker.cs
+++ Coordinator/server/src/Entities/Worker.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using ServiceStack.DataAnnotations;
 
 namespace Server
 {
 	public class Worker
 	{
-		[AutoIncrement]
 		public long Id { get; set; }
 		public string Address { get; set; }
 		public bool Alive { get; set; }
