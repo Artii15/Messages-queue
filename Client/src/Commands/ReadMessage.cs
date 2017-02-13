545bfc7d979253c2d7192c50e29ea1629b5bdfe7
Artii15 <artur.kowalski15@gmail.com>
2017-02-13 14:53:51 +0100

    Reading messages from queue

--- Client/src/Commands/ReadMessage.cs
+++ Client/src/Commands/ReadMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using Client.Entities;
 using RestSharp;
 
 namespace Client.Commands
@@ -11,13 +12,51 @@ namespace Client.Commands
 		public override void Execute()
 		{
 			Console.Write("Queue name: ");
-
+			try
+			{
+				Console.WriteLine(ReadQueue(Reader.ReadNonEmptyString()));
+			}
+			catch (ArgumentException)
+			{
+				Console.WriteLine("Invalid queue name");
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Something went wrong");
+			}
 		}
 
-		HttpStatusCode SendRequest(string queueName)
+		ReadMessageResponse ReadQueue(string queueName)
+		{
+			ReadMessageResponse message = null;
+			while (message == null)
+			{
+				var readResponse = SendReadRequest(queueName);
+				message = TryConfirmRead(queueName, readResponse.Data) ? null : readResponse.Data;
+			}
+			return message;
+		}
+
+		IRestResponse<ReadMessageResponse> SendReadRequest(string queueName)
 		{
 			var request = new RestRequest($"queues/{queueName}/messages", Method.GET);
-			return Client.Execute(request).StatusCode;
+			var response = Client.Execute<ReadMessageResponse>(request);
+
+			switch (response.StatusCode)
+			{
+				case HttpStatusCode.OK:
+					return response;
+				case HttpStatusCode.NotFound:
+					throw new ArgumentException();
+				default:
+					throw new Exception();
+			}
+		}
+
+		bool TryConfirmRead(string queueName, ReadMessageResponse readResponse)
+		{
+			var request = new RestRequest($"queues/{queueName}/messages/{readResponse.Id}", Method.DELETE);
+			return Client.Execute(request).StatusCode == HttpStatusCode.NoContent;
 		}
 	}
 }
