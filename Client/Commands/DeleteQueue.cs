525355fe8eb47a7daa0b2babd95fd72c6d8387cc
Artii15 <artur.kowalski15@gmail.com>
2017-02-13 13:01:20 +0100

    Deleting queue

---
+++ Client/Commands/DeleteQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using RestSharp;
+
+namespace Client.Commands
+{
+	public class DeleteQueue: Command
+	{
+		public DeleteQueue(RestClient client) : base(client) { }
+
+		public override void Execute()
+		{
+			Console.Write("Queue name: ");
+			switch (SendCreateRequest(Reader.ReadNonEmptyString()))
+			{
+				case HttpStatusCode.NoContent:
+					Console.WriteLine("Queue deleted");
+					break;
+				case HttpStatusCode.NotFound:
+					Console.WriteLine("Queue with provided name not exists");
+					break;
+				default:
+					Console.WriteLine("Something went wrong");
+					break;
+			}
+		}
+
+		HttpStatusCode SendCreateRequest(string queueName)
+		{
+			var request = new RestRequest($"queues/{queueName}", Method.DELETE);
+			return Client.Execute(request).StatusCode;
+		}
+	}
+}
