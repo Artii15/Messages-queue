15618970e0747b288197f73f3e9654fb69be3bc9
Artii15 <artur.kowalski15@gmail.com>
2017-02-13 11:53:28 +0100

    Client

---
+++ Client/Client/Commands/CreateQueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Client.Commands
+{
+	public class CreateQueue
+	{
+		public void Create()
+		{
+			Console.Write("Queue name: ");
+			var queueName = Console.ReadLine();
+		}
+	}
+}
