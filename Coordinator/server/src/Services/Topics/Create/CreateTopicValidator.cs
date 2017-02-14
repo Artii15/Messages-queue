4b23eea7783e6b99630f711850a6f04d1c9a6986
Pyre540 <kamil540@gazeta.pl>
2017-02-14 12:53:27 +0100

    topic and queue name validation

---
+++ Coordinator/server/src/Services/Topics/Create/CreateTopicValidator.cs
@@ -0,0 +1,14 @@
+﻿using ServiceStack.FluentValidation;
+using ServiceStack.ServiceInterface;
+
+namespace Server
+{
+	public class CreateTopicValidator : AbstractValidator<CreateTopic>
+	{
+		public CreateTopicValidator() 
+		{
+			RuleSet(ApplyTo.Post, () => { RuleFor(r => r.Name).NotEmpty().WithErrorCode("ShouldNotBeEmpty"); });
+			RuleSet(ApplyTo.Post, () => { RuleFor(r => r.Name).Matches("^[a-zA-Z0-9]+$").WithErrorCode("InvalidName"); });
+		}
+	}
+}
