68b6c9629eac753b93e058d68157e6428ada2c1d
Pyre540 <kamil540@gazeta.pl>
2017-02-14 18:16:21 +0100

    basic topic operation

---
+++ Coordinator/server/src/Logic/BasicTopicOperation.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using System.Net;
+using RestSharp;
+
+namespace Server
+{
+	public class BasicTopicOperation : BasicOperation
+	{
+		public BasicTopicOperation(IDbConnection dbConnection) : base(dbConnection)
+		{
+		}
+
+		protected void processRequest(string topicName, RestRequest request)
+		{
+			if (!TopicsQueries.TopicExists(DBConnection, topicName))
+				throw new TopicNotExistsException();
+			else
+			{
+				var topic = TopicsQueries.getTopicByName(DBConnection, topicName);
+				var worker = WorkerQueries.GetWorkerById(DBConnection, topic.Worker);
+				var coworker = WorkerQueries.GetWorkerById(DBConnection, topic.Cooperator);
+
+				PropageteRequestToWorkers(request, topic, worker, coworker);
+			}
+		}
+
+		protected void PropageteRequestToWorkers(RestRequest request, Topic topic, Worker worker, Worker coworker)
+		{
+			var requestForMaster = request.AddParameter("Cooperator", coworker.Address);
+			IRestResponse response;
+
+			if (WorkerQueries.IsWorkerAlive(DBConnection, worker.Id))
+			{
+				response = PropagateRequest(requestForMaster, worker);
+				if (response.ResponseStatus == ResponseStatus.TimedOut ||
+					response.ResponseStatus == ResponseStatus.Error)
+				{
+					response = PropagateRequest(request, coworker);
+					TopicsQueries.swapWorkers(DBConnection, topic);
+				}
+			}
+			else
+			{
+				response = PropagateRequest(request, coworker);
+				TopicsQueries.swapWorkers(DBConnection, topic);
+			}
+			if (response.StatusCode != HttpStatusCode.OK)
+				throw new BadRequestException();
+		}
+
+		IRestResponse PropagateRequest(IRestRequest request, Worker worker)
+		{
+			var client = new RestClient(worker.Address);
+			client.Timeout = TIMEOUT;
+			return client.Execute(request);
+		}
+	}
+}
