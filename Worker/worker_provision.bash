c80ff760c5e22e29862e853967f20802725fe4cd
Artii15 <artur.kowalski15@gmail.com>
2017-02-15 12:02:51 +0100

    Provisioning scripts

---
+++ Worker/worker_provision.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+WORKER_ID=1
+SERVICE_INTERFACE="wlp3s0"
+SERVICE_PORT=8888
+COORDINATOR_ADDRESS="http://localhost:8080"
+
+PROJECT_REPO_NAME="Messages-queue"
+PROJECT_REPO="https://github.com/Artii15/$PROJECT_REPO_NAME.git"
+PROJECT_DIR_PWD="/home/artur/Pulpit"
+PROJECT_DIR="$PROJECT_DIR_PWD/$PROJECT_REPO_NAME"
+
+if [ -d $PROJECT_DIR ]; then
+    cd $PROJECT_DIR
+    git pull origin master
+else
+    cd $PROJECT_DIR_PWD
+    git clone $PROJECT_REPO
+fi
+
+WORKER_DIR="$PROJECT_DIR/Worker"
+
+cd $WORKER_DIR
+
+WORKER_SLN="worker.sln"
+
+nuget restore $WORKER_SLN
+xbuild /p:Configuration=Release $WORKER_SLN
+
+ENV_FILE="$WORKER_DIR/server/bin/Release/.env"
+
+echo "WORKER_ID=$WORKER_ID" > $ENV_FILE
+echo "COORDINATOR_ADDRESS=$COORDINATOR_ADDRESS" >> $ENV_FILE
+echo "SERVICE_INTERFACE=$SERVICE_INTERFACE" >> $ENV_FILE
+echo "SERVICE_PORT=$SERVICE_PORT" >> $ENV_FILE
---
+++ Worker/worker_run.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WORKER_RUNTIME_DIR="/home/artur/Pulpit/Messages-queue/Worker/server/bin/Release"
+
+cd $WORKER_RUNTIME_DIR
+
+while read ENV_VARIABLE
+do
+    eval $ENV_VARIABLE
+done < .env
+
+IP_ADDRESS=$(ip address show dev $SERVICE_INTERFACE | grep inet | grep -v inet6 | sed -e 's/^[ \t]*//' | cut -d " " -f2 | cut -d "/" -f1)
+
+SERVICE_ADDRESS="http://$IP_ADDRESS:$SERVICE_PORT"
+
+export WORKER_ID
+export COORDINATOR_ADDRESS
+./server.exe $SERVICE_ADDRESS
